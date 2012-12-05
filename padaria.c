6846ae661ed5cde3d7d7da490e09be150196cb6b
Salviano Ludgério <salviano.lfg@gmail.com>
2012-12-05 18:47:10 -0200

    Refactoring: remove global variables

--- padaria.c
+++ padaria.c
@@ -12,34 +12,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "minunit.h"
 
-float products_prices[100];
-int products_sold[100];
-int length = sizeof(products_sold) / sizeof(int);
+int tests_run = 0;
+int length = 0;
 
-void read_prices_products_from_a_file(char *name_of_file) {
+void read_prices_products_from_a_file(char *name_of_file, float *prices) {
   FILE *in = fopen(name_of_file, "rt");
   char buffer[100];
 
   int i = 0;
   for (i = 0; i < length; i++) {
     fgets(buffer, 20, in);
-    products_prices[i] = atof(buffer);
+    prices[i] = atof(buffer);
   }
 
   fclose(in);
 }
 
-void generate_random_values_for_products_sold(int max_value) {
+void generate_random_values_for_products_sold(int max_value, int *products_sold) {
   int i = 0;
   srand(time(NULL));
 
   for (i = 0; i < length; i++) {
     products_sold[i] = rand() % max_value;
+    /* products_sold[i] = 1; */
   }
 }
 
-float billing_of_month() {
+float billing_of_month(float *products_prices, int *products_sold) {
   float total_billing = 0;
   int i = 0;
 
@@ -50,13 +51,66 @@ float billing_of_month() {
   return total_billing;
 }
 
+/* static char * test_length_of_products_sold() { */
+/*   mu_assert("error, length != 100", length == 100); */
+/*   return 0; */
+/* } */
+
+/* static char * test_generate_random_values() { */
+/*   generate_random_values_for_products_sold(2); */
+/*  */
+/*   int i = 0; */
+/*   for (i = 0; i < length; i++) { */
+/*     #<{(| mu_assert("error, generate_random_values_for_products_sold", |)}># */
+/*         #<{(| products_sold[i] == 1); |)}># */
+/*   } */
+/*   return 0; */
+/* } */
+
+/* static char * test_billing_of_month() { */
+/*   char *file = "lista_precos.txt"; */
+/*   #<{(| read_prices_products_from_a_file(file); |)}># */
+/*  */
+/*   generate_random_values_for_products_sold(2); */
+/*  */
+/*   mu_assert("error, billing_of_month != 485.899780", */
+/*       billing_of_month() == 485.899780); */
+/*  */
+/*   return 0; */
+/* } */
+
+/* static char * all_tests() { */
+/*   mu_run_test(test_length_of_products_sold); */
+/*   mu_run_test(test_generate_random_values); */
+/*   #<{(| mu_run_test(test_billing_of_month); |)}># */
+/*  */
+/*   return 0; */
+/* } */
+
+/* int main(int argc, char **argv) { */
+/*   char *result = all_tests(); */
+/*   if (result != 0) { */
+/*     printf("%s\n", result); */
+/*   } */
+/*   else { */
+/*     printf("ALL TESTS PASSED\n"); */
+/*   } */
+/*   printf("Tests run: %d\n", tests_run); */
+/*  */
+/*   return result != 0; */
+/* } */
+
 int main(int argc, const char *argv[]) {
+  float prices[100];
+  int   sold[100];
+  length = sizeof(sold) / sizeof(int);
+
   char *file = "lista_precos.txt";
-  read_prices_products_from_a_file(file);
+  read_prices_products_from_a_file(file, prices);
 
-  generate_random_values_for_products_sold(5);
+  generate_random_values_for_products_sold(2, sold);
 
-  printf("Faturamento mensal: R$ %g reais.\n" , billing_of_month());
+  printf("Faturamento mensal: R$ %g reais.\n" , billing_of_month(prices, sold));
 
   return 0;
 }
